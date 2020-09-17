410035ceb43d793f6716ae1399e6407288430d0d
paramee0598 <63010598@kmitl.co.th>
2020-09-17 23:45:39 +0700

    edita Error A-Z *1

--- 1803/Source.cpp
+++ 1803/Source.cpp
@@ -3,10 +3,11 @@ int main() {
     int i, j;
     char input, alphabet = 'a';
     char Alphabet = 'A';
-    printf("Enter an uppercase character you want to print in the last row: ");
+    printf("Enter an a-z OR A-Z row: ");
     scanf_s("%c", &input);
     if (input >= 'a' & input <= 'z' || input >= 'A' & input <= 'Z') {
     printf("*\n");
+    if (input >= 'a' & input <= 'z'){
         for (i = 1; i <= (input - 'a' + 1); ++i) {
             for (j = 1; j <= i; ++j) {
                 printf("%c ", Alphabet);
@@ -16,6 +17,18 @@ int main() {
             ++alphabet;
             printf("\n");
         }
+    }
+    if(input >= 'A' & input <= 'Z') {
+        for (i = 1; i <= (input - 'A' + 1); ++i) {
+            for (j = 1; j <= i; ++j) {
+                printf("%c ", Alphabet);
+                printf("%c ", alphabet);
+            }
+            ++Alphabet;
+            ++alphabet;
+            printf("\n");
+        }
+    }
     }
     else printf("Error ");
     return 0;
